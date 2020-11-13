81ef324d075d6f7805c033442bb1abc03db0985f
David Daniels <dldaniels07@yahoo.com>
2020-11-13 16:49:03 -0700

    Configure Boot application

--- JPARun/src/test/java/com/skilldistillery/run/entities/RunTest.java
+++ JPARun/src/test/java/com/skilldistillery/run/entities/RunTest.java
@@ -1,6 +1,11 @@
 package com.skilldistillery.run.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -9,26 +14,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RunTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Run run;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("RunPU");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		run = em.find(Run.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		em.close();
+		run = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_Run_entity_mapping() {
+		assertNotNull(run);
+		assertEquals("Phil", run.getName());
 	}
 
 }
