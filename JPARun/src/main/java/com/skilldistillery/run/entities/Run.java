00e51b63122d3b911593e0e927ed215e0724caa0
David Daniels <dldaniels07@yahoo.com>
2020-11-14 20:20:56 -0700

    DB additions

--- JPARun/src/main/java/com/skilldistillery/run/entities/Run.java
+++ JPARun/src/main/java/com/skilldistillery/run/entities/Run.java
@@ -1,20 +1,76 @@
 package com.skilldistillery.run.entities;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "table1")
 public class Run {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	private String name;
+
+	private String location;
+
+	private double distance;
+
+	private double time;
+
+	private int calories;
+
+	private double elevationGain;
+
+	private Date date;
+
+	public Run() {
+		super();
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
+	public int getCalories() {
+		return calories;
+	}
+
+	public void setCalories(int calories) {
+		this.calories = calories;
+	}
+
+	public double getElevationGain() {
+		return elevationGain;
+	}
+
+	public void setElevationGain(double elevationGain) {
+		this.elevationGain = elevationGain;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
 
 	public int getId() {
 		return id;
@@ -32,16 +88,30 @@ public class Run {
 		this.name = name;
 	}
 
-	public Run() {
-		super();
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + calories;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(distance);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(elevationGain);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
 		result = prime * result + id;
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		temp = Double.doubleToLongBits(time);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
 		return result;
 	}
 
@@ -54,18 +124,42 @@ public class Run {
 		if (getClass() != obj.getClass())
 			return false;
 		Run other = (Run) obj;
+		if (calories != other.calories)
+			return false;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (Double.doubleToLongBits(distance) != Double.doubleToLongBits(other.distance))
+			return false;
+		if (Double.doubleToLongBits(elevationGain) != Double.doubleToLongBits(other.elevationGain))
+			return false;
 		if (id != other.id)
+			return false;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
 			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
+		if (Double.doubleToLongBits(time) != Double.doubleToLongBits(other.time))
+			return false;
 		return true;
 	}
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Run [id=").append(id).append(", name=").append(name).append(", location=").append(location)
+				.append(", distance=").append(distance).append(", time=").append(time).append(", calories=")
+				.append(calories).append(", elevationGain=").append(elevationGain).append(", date=").append(date)
+				.append("]");
+		return builder.toString();
+	}
 
 }
