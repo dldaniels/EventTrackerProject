204e230d0f7471c7669212fe6b5aa3bb577cd7f0
David Daniels <dldaniels07@yahoo.com>
2020-11-14 22:24:39 -0700

    all runs working on postman

--- RunTracker/src/main/java/com/skilldistillery/run/controllers/RunController.java
+++ RunTracker/src/main/java/com/skilldistillery/run/controllers/RunController.java
@@ -1,10 +1,13 @@
 package com.skilldistillery.run.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.run.entities.Run;
 import com.skilldistillery.run.services.RunService;
 
 @RequestMapping("api")
@@ -19,6 +22,9 @@ public class RunController {
 		return "pong!";
 	}
 	
-	
+	@RequestMapping(path = "allruns") 
+	public List<Run> allRuns() {
+		return svc.getAllRuns();
+	}
 
 }
